ccc87fd31097dc6d6818d30802caeaea5b939b81
victorernan <victor.ospina.giraldo@gmail.com>
2020-08-03 23:55:01 +0200

    Delete app-router.ts

--- src/app/app-router.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-//import { TemplateComponent } from './pages/template/template.component';
-import { ReactiveComponent } from './pages/reactive/reactive.component';
-
-export const ROUTES: Routes = [
-  // { path: 'templete', component: TemplateComponent },
-  { path: 'reactivo', component: ReactiveComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'reactivo' },
-];
